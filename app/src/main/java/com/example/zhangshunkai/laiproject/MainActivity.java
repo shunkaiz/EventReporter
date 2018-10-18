b55fe997618cb74eb9bd8ab83414754628d55a64
shunkaiz <shunkaiz@usc.edu>
2018-10-18 01:29:31 -0700

    register/login function s

--- app/src/main/java/com/example/zhangshunkai/laiproject/MainActivity.java
+++ app/src/main/java/com/example/zhangshunkai/laiproject/MainActivity.java
@@ -1,22 +1,100 @@
 package com.example.zhangshunkai.laiproject;
 
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements EventFragment.OnItemSelectListener,
-        CommentFragment.OnItemSelectListener {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity{
+
+    private EditText mUsernameEditText;
+    private EditText mPasswordEditText;
+    private Button mSubmitButton;
+    private Button mRegisterButton;
+    private DatabaseReference mDatabase;
 
 
-    private EventFragment mEventFragment;
-    private CommentFragment mCommentFragment;
+
+/*    private EventFragment mEventFragment;
+    private CommentFragment mCommentFragment;*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mEventFragment = new EventFragment();
+
+        // Firebase uses singleton to initialize the sdk
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mUsernameEditText = (EditText) findViewById(R.id.editTextLogin);
+        mPasswordEditText = (EditText) findViewById(R.id.editTextPassword);
+        mSubmitButton = (Button) findViewById(R.id.submit);
+        mRegisterButton = (Button) findViewById(R.id.register);
+
+        // register button
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = mUsernameEditText.getText().toString();
+                final String password = Utils.md5Encryption(mPasswordEditText.getText().toString());
+                final User user = new User(username, password, System.currentTimeMillis());
+                mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(username)) {
+                            Toast.makeText(getBaseContext(),"username is already registered, please change one", Toast.LENGTH_SHORT).show();
+                        } else if (!username.equals("") && !password.equals("")){
+                            // put username as key to set value
+                            mDatabase.child("users").child(user.getUsername()).setValue(user);
+                            Toast.makeText(getBaseContext(),"Successfully registered", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+        // login button
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = mUsernameEditText.getText().toString();
+                final String password = Utils.md5Encryption(mPasswordEditText.getText().toString());
+                mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(username) && (password.equals(dataSnapshot.child(username).child("password").getValue()))) {
+                            Log.i( " Your log", "You successfully login");
+                        } else {
+                            Toast.makeText(getBaseContext(),"Please login again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+/*        mEventFragment = new EventFragment();
         mCommentFragment = new CommentFragment();
 //        ListView eventList = findViewById(R.id.event_list);
 //        //ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.event_item, R.id.event_title, getEventNames());
@@ -30,7 +108,7 @@ public class MainActivity extends AppCompatActivity implements EventFragment.OnI
 
         if(findViewById(R.id.comment_container)!=null && isTablet()){
             getSupportFragmentManager().beginTransaction().add(R.id.comment_container, mCommentFragment).commit();
-        }
+        }*/
 
     }
 
@@ -43,6 +121,7 @@ public class MainActivity extends AppCompatActivity implements EventFragment.OnI
 
 
 
+/*
     @Override
     public void onItemSelected(int position){
         if(!isTablet()){
@@ -62,5 +141,6 @@ public class MainActivity extends AppCompatActivity implements EventFragment.OnI
     }
 
 
+*/
 
 }
